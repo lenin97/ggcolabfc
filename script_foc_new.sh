fdf7096e1836500b94cd9e556b99b2f62ffb1c59
lenin97 <lenin97@users.noreply.github.com>
2025-01-13 22:46:31 +0000

    Update script_foc_new.sh

--- script_foc_new.sh
+++ script_foc_new.sh
@@ -1,7 +1,7 @@
 !pip install pygit2==1.15.1
 %cd /content
 !git clone https://github.com/lllyasviel/Fooocus.git
+!git clone https://github.com/lenin97/conf_file_fcai.git
 %cd /content/Fooocus
 !python entry_with_update.py --share --always-high-vram
-!git clone https://github.com/lenin97/conf_file_fcai.git
 !mv /content/conf_file_fcai/config.txt /content/Fooocus/config.txt
